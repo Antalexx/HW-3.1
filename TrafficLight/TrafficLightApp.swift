15b43da138494e3a3e75843f72ee2a3526d3edb4
Антоненко Александр <aalexanto@gmail.com>
2021-07-28 17:34:32 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Alexander Antonenko on 28.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
